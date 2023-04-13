4e5031a3528d29ef0c3ca6974d3724b23ff6b8c6
thanghoang0163 <59225762+thanghoang0163@users.noreply.github.com>
2023-04-13 18:35:26 +0700

    Update Sign Screen

---
+++ www/page/sign/index.js
@@ -0,0 +1,29 @@
+var $ = document.querySelector.bind(document);
+var $$ = document.querySelectorAll.bind(document);
+
+const items = $$(".sign-item");
+items[0].classList.add("item--border");
+items.forEach((element) => {
+  element.addEventListener("click", () => {
+    const item = $(".sign-item.item--border");
+    item.classList.remove("item--border");
+    element.classList.add("item--border");
+  });
+});
+
+const signIn = $(".sign-item:first-child");
+const signUp = $(".sign-item:last-child");
+const signContent = $(".sign-content");
+
+signIn.addEventListener("click", () => {
+  signContent.style = "transform: translateX(0)";
+});
+
+signUp.addEventListener("click", () => {
+  signContent.style = "transform: translateX(-100vw)";
+});
+
+const button = $(".sign-button");
+button.addEventListener("click", () => {
+  location.replace("/page/splash/index.html");
+});
