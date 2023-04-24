95bbb581c596b7be960b8454144a15db9fa42412
thanghoang0163 <59225762+thanghoang0163@users.noreply.github.com>
2023-04-24 18:13:21 +0700

    Edit Profile Screen

--- www/page/profile/index.js
+++ www/page/profile/index.js
@@ -2,7 +2,23 @@ var $ = document.querySelector.bind(document);
 var $$ = document.querySelectorAll.bind(document);
 
 const backBtn = $(".back-button");
+const editBtns = $$(".profile-edit-btn");
+const inputs = $$(".profile-input");
 
 backBtn.addEventListener("click", () => {
   history.go(-1);
+});
+
+editBtns.forEach((edit, index) => {
+  edit.addEventListener("click", () => {
+    inputs.forEach((input, idx) => {
+      index == idx ? input.focus() : null;
+    });
+  });
+});
+
+const updateBtn = $(".update-btn");
+
+updateBtn.addEventListener("click", () => {
+  window.location.href = "/page/product/index.html";
 });
