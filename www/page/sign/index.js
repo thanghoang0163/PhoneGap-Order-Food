8f157978664db928432d6f5a819d84ff912ac879
thanghoang0163 <59225762+thanghoang0163@users.noreply.github.com>
2023-05-19 20:44:02 +0700

    Handle Sign in Section in Sign Screen

--- www/page/sign/index.js
+++ www/page/sign/index.js
@@ -14,6 +14,10 @@ items.forEach((element) => {
 const signIn = $(".sign-item:first-child");
 const signUp = $(".sign-item:last-child");
 const signContent = $(".sign-content");
+const signInBtn = $(".sign-button");
+const phoneInput = $(".sign-input[type=text]");
+const passInput = $(".sign-input[type=password]");
+const checkText = $(".check-sign");
 
 signIn.addEventListener("click", () => {
   signContent.style = "transform: translateX(0)";
@@ -22,3 +26,23 @@ signIn.addEventListener("click", () => {
 signUp.addEventListener("click", () => {
   signContent.style = "transform: translateX(-100vw)";
 });
+
+async function logJSONData() {
+  const response = await fetch("https://testapi.io/api/thanghoang/sign");
+  const jsonData = await response.json();
+  signInBtn.addEventListener("click", () => {
+    if (phoneInput.value == jsonData.user.phone) {
+      if (passInput.value == jsonData.user.password) {
+        window.location.href = "/page/product/index.html";
+      } else {
+        checkText.style.display = "block";
+        checkText.innerHTML = "Mật khẩu sai!";
+      }
+    } else {
+      checkText.style.display = "block";
+      checkText.innerHTML = "Số điện thoại hoặc mật khẩu sai!";
+    }
+  });
+}
+
+logJSONData();
