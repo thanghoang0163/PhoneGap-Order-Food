7e3d94f3fc3278fac29652717a1486597d8dcbce
thanghoang0163 <59225762+thanghoang0163@users.noreply.github.com>
2023-05-19 20:45:15 +0700

    Render Products with API in Product Screen

--- www/page/product/index.js
+++ www/page/product/index.js
@@ -1,4 +1,4 @@
-import { database } from "./firebase.js";
+// import { database } from "./firebase.js";
 // import { ref, child, get } from "firebase/database";
 
 var $ = document.querySelector.bind(document);
@@ -25,19 +25,50 @@ closeBtn.addEventListener("click", () => {
 
 price.innerHTML = formatNumber(50000);
 
-const getAPI = () => {
-  const dbRef = ref(database);
-  get(child(dbRef, `user/1`))
-    .then((snapshot) => {
-      if (snapshot.exists()) {
-        console.log(snapshot.val());
-      } else {
-        console.log("No data available");
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-};
+// const getAPI = () => {
+//   const dbRef = ref(database);
+//   get(child(dbRef, `user/1`))
+//     .then((snapshot) => {
+//       if (snapshot.exists()) {
+//         console.log(snapshot.val());
+//       } else {
+//         console.log("No data available");
+//       }
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// };
 
 // getAPI();
+
+async function logJSONData() {
+  // main.js
+
+  // POST request using fetch()
+  fetch("https://testapi.io/api/thanghoang/sign", {
+    // Adding method type
+    method: "PUT",
+
+    // Adding body or contents to send
+    body: JSON.stringify({
+      user: {
+        phone: "0123456789",
+        password: "121",
+      },
+    }),
+
+    // Adding headers to the request
+    headers: {
+      Accept: "application/json",
+      "Content-type": "application/json",
+    },
+  })
+    // Converting to JSON
+    .then((response) => response.json())
+
+    // Displaying results to console
+    .then((json) => console.log(json));
+}
+
+logJSONData();
