39fefdd8af7149f26d7b9de461c9a7d0c1fc3aa0
thanghoang0163 <59225762+thanghoang0163@users.noreply.github.com>
2023-06-18 17:30:14 +0700

    Delete Product Detail Screen

--- www/page/product-detail/index.js
+++
@@ -1,8 +0,0 @@
-var $ = document.querySelector.bind(document);
-var $$ = document.querySelectorAll.bind(document);
-
-const backBtn = $(".back-button");
-
-backBtn.addEventListener("click", () => {
-  history.go(-1);
-});
