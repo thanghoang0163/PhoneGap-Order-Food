2c7ee1fcd7497ec6dde96065d83952ee06efab36
thanghoang0163 <59225762+thanghoang0163@users.noreply.github.com>
2023-06-19 11:52:50 +0700

    Edit Checkout Screen

--- www/page/checkout/index.js
+++ www/page/checkout/index.js
@@ -3,11 +3,31 @@ var $$ = document.querySelectorAll.bind(document);
 
 const backBtn = $(".back-button");
 const checkoutBtn = $(".checkout-btn");
+const paymentBtn = $$(".checkout-payment-btn");
+const deliveryBtn = $$(".checkout-delivery-btn");
+var payment = { title: "Thẻ ATM" };
+var delivery = { title: "Giao đến cửa" };
+
+const checkRadioBtn = (btn, obj) => {
+  btn.forEach((item) => {
+    item.addEventListener("click", () => {
+      if (item.checked) {
+        obj = { title: item.value };
+        console.log(obj);
+      }
+    });
+  });
+};
+
+checkRadioBtn(paymentBtn, payment);
+checkRadioBtn(deliveryBtn, delivery);
+
+checkoutBtn.addEventListener("click", () => {
+  localStorage.setItem("payment", JSON.stringify(payment));
+  localStorage.setItem("delivery", JSON.stringify(delivery));
+  window.location.href = "/page/compete/index.html";
+});
 
 backBtn.addEventListener("click", () => {
   history.go(-1);
-});
-
-checkoutBtn.addEventListener("click", () => {
-  window.location.href = "/page/compete/index.html";
 });
