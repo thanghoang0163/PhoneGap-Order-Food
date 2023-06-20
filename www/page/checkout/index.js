349d6b71d3f6fc7d2e6e27a4b0775ea73bb701ae
thanghoang0163 <59225762+thanghoang0163@users.noreply.github.com>
2023-06-20 19:24:19 +0700

    Finish Handling Method in Checkout Screen

--- www/page/checkout/index.js
+++ www/page/checkout/index.js
@@ -5,14 +5,17 @@ const backBtn = $(".back-button");
 const checkoutBtn = $(".checkout-btn");
 const paymentBtn = $$(".checkout-payment-btn");
 const deliveryBtn = $$(".checkout-delivery-btn");
-var payment = { title: "Thẻ ATM" };
-var delivery = { title: "Giao đến cửa" };
+var payment = { title: "Thẻ ATM", method: "payment" };
+var delivery = { title: "Giao đến cửa", method: "delivery" };
 
 const checkRadioBtn = (btn, obj) => {
   btn.forEach((item) => {
     item.addEventListener("click", () => {
       if (item.checked) {
-        obj = { title: item.value };
+        obj = { ...obj, title: item.value };
+        obj.method == "payment"
+          ? localStorage.setItem("payment", JSON.stringify(obj))
+          : localStorage.setItem("delivery", JSON.stringify(obj));
         console.log(obj);
       }
     });
@@ -23,8 +26,6 @@ checkRadioBtn(paymentBtn, payment);
 checkRadioBtn(deliveryBtn, delivery);
 
 checkoutBtn.addEventListener("click", () => {
-  localStorage.setItem("payment", JSON.stringify(payment));
-  localStorage.setItem("delivery", JSON.stringify(delivery));
   window.location.href = "/page/compete/index.html";
 });
 
